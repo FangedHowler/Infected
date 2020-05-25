86ada33201ba3ab883b2c9b622ed63fdd8562b15
Danny-O-dev <58507146+Danny-O-dev@users.noreply.github.com>
2020-05-25 10:39:57 +0200

    Marsh Cavern (WIP)
    Added some new objects and some new textures. The island has been added. Light setting changed a bit. Marsh Cavern has been added still a working progress.

---
+++ Conversation System/Assets/Scripts/DialogueSystem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueSystem : MonoBehaviour
+{
+    public Text Dialoguesystem;
+
+    void Update()
+    {
+        Text *= 1 + 1;
+    }
+
+    void Update()
+    {
+        Text *= 2 + 1; 
+    }
+}
