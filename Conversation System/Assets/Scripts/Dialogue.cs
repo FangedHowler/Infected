0cfed9096a6b4f6f3341998d5dc7c8bcb09a1c4a
Danny-O-dev <58507146+Danny-O-dev@users.noreply.github.com>
2020-05-26 10:31:07 +0200

    Island moved to level 1
    Island moved to level 1

---
+++ Conversation System/Assets/Scripts/Dialogue.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dialogue : MonoBehaviour
+{
+    public int pressYes;
+    public int pressNo;
+    public int line;
+    public Text dialogueText;
+    public Text Yes;
+    public Text No;
+
+    public void PressYes()
+    {
+        //    line *= 1 + 1;
+        //Yes = transform.Find("Yes").GetComponent<Text>();
+        pressYes += 1;
+        if (pressYes == 1)
+        {
+            dialogueText.text = "Is goed dan. Wat vindt je van Rood?";
+            Yes.text = "Mooi";
+            No.text = "Lelijk";
+        }
+        if (pressYes == 2)
+        {
+            dialogueText.text = "Wat vind je dan van oranje?";
+        }
+    }
+    public void PressNo()
+    {
+        //  line *= 2 + 1;
+        pressNo += 1;
+        if (pressNo == 1)
+        {
+            dialogueText.text = "Vind je blauw leuk?";
+        }
+        if (pressNo == 2)
+        {
+            dialogueText.text = "Wat vind je dan van groen?";
+
+        }
+        if (pressNo == 3)
+        {
+            dialogueText.text = "Wat vind je dan van geel?";
+
+        }
+    }
+}
